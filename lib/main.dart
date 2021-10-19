21c16b2d1d3f704ea6442d7a275998b05a05cdf4
ricarthlima <rrsl@cin.ufpe.br>
2021-10-19 12:01:00 -0300

    firebase and firestore working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -19,14 +24,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  TextEditingController _textController = TextEditingController();
+  List<String> listNames = [];
+
+  @override
+  void initState() {
+    refresh();
+
+    db.collection("contacts").snapshots().listen((snapshot) {
+      setState(() {
+        listNames = [];
+        snapshot.docs.forEach((document) {
+          listNames.add(document.get("name"));
+        });
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Minha pequena agenda"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => refresh(),
+        child: Icon(Icons.refresh),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 64),
@@ -38,13 +69,56 @@ class HomeScreen extends StatelessWidget {
               style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
-            TextField(),
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(labelText: "Insira um nome"),
+            ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => sendData(),
               child: Text("Enviar"),
             ),
+            SizedBox(
+              height: 16,
+            ),
+            (listNames.length == 0)
+                ? Text(
+                    "Nenhum contato registrado",
+                    style: TextStyle(color: Colors.grey),
+                    textAlign: TextAlign.center,
+                  )
+                : Column(
+                    children: [
+                      for (String s in listNames) Text(s),
+                    ],
+                  )
           ],
         ),
+      ),
+    );
+  }
+
+  void refresh() async {
+    QuerySnapshot query = await db.collection("contacts").get();
+
+    listNames = [];
+    query.docs.forEach((document) {
+      print(document.id); //Mostrar o id que escolhemos
+      String data = document.get("name");
+      setState(() {
+        listNames.add(data);
+      });
+    });
+  }
+
+  void sendData() {
+    String id = Uuid().v1();
+    db.collection("contacts").doc(id).set({"name": _textController.text});
+
+    //Visual Feedback
+    _textController.text = "";
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Salvo no Firestore!"),
       ),
     );
   }
